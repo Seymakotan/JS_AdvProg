d604037637a5bb70af7e859eb9806c483ffe3174
Seymakotan <33803468+Seymakotan@users.noreply.github.com>
2020-12-29 16:46:00 +0300

    Update quiz.js

--- Term_project_99/quiz.js
+++ Term_project_99/quiz.js
@@ -44,10 +44,71 @@ let sonucgifs=[
     "https://seymakotan.github.io/JS_AdvProg/Term_project_99/gif/sonuc_boyle.gif",
     "https://seymakotan.github.io/JS_AdvProg/Term_project_99/gif/sonuc_terry.gif",
 ]
-
+let aciklama=["Sen adeta ciddiyetin sembolüsün. Yüzünde hiç mi mimik oynamaz, hiç mi gülümsemez ya da gülmezsin sen? Ya bakma şakaya vurduğumuza sen de gülüp eğleniyorsun tabii ama bu kadar çok kasma her şeyi, biraz rahatla lütfen. Belki gevşemek sana korkunç bir kelime gibi geliyor ama gerçekten biraz gevşesen ruhun ne kadar rahatlayacak haberin yok. "]
 var options;
 var testsonuc;
 
+function puanlama(){
+
+    options = document.getElementsByName("option");
+    for(var i=0; i<options.length; i++){
+        if(options[i].checked){
+
+            if(i==0){
+                toplamA++;
+            }else if(i==1){
+                toplamB++;
+            }else if (i==2){
+                toplamC++;
+            }else if (i==3){
+                toplamD++;
+            }
+           
+        }
+    }
+    if(sayi==9){
+        document.getElementById("quizekran").style.visibility = "hidden" ;
+        document.getElementById("ekran3").style.visibility = "visible" ;
+
+        console.log(toplam);
+        if(toplamA==4 || (toplamA>=toplamB &&toplamA >=toplamC &&toplamA>=toplamD )){
+            testsonuc="Raymond Holt "
+            console.log("holt");
+            document.getElementById("resultImg").src = sonucgifs[0];
+            document.getElementById("resultText1").innerText = "Reymond Holt";
+            
+
+        }else if(toplamB==4 || (toplamB>toplamA &&toplamB>toplamC &&toplamB>toplamD )){
+            testsonuc="Jake Peralta "
+            console.log("jake");
+            document.getElementById("resultImg").src = sonucgifs[1];
+            document.getElementById("resultText1").innerText = "Jake Peralta";
+
+        }else if(toplamC==4 || (toplamC>toplamA &&toplamC>toplamB &&toplamC>toplamD )){
+            testsonuc="Charles Boyle "
+            console.log("Boyle");
+            document.getElementById("resultImg").src = sonucgifs[2];
+            document.getElementById("resultText1").innerText = "Charles Boyle";
+
+        }else if(toplamD==4 || (toplamD>toplamA &&toplamD>toplamB &&toplamD>toplamC)){
+            console.log("Terry Jeffords");
+            document.getElementById("resultImg").src = sonucgifs[3];
+            document.getElementById("resultText1").innerText = "Terry Jeffords";
+            
+        }
+
+        
+       
+   
+
+    }
+    //console.log("toplam puan :" +toplam);
+    console.log("A : " +toplamA);
+    console.log("B : " +toplamB);
+    console.log("C : " +toplamC);
+    console.log("D : " +toplamD);
+
+}
 
 
 
@@ -65,28 +126,43 @@ function degistir(){
         }
     }
     sayi++;
-
+    //puanlama();
+    puanlama();
     op0.checked = false;
     op1.checked = false;
     op2.checked = false;
     op3.checked = false;
-  
-    if(sayi==9){
-        document.getElementById("quizekran").style.visibility = "hidden" ;
-        document.getElementById("ekran3").style.visibility = "visible" ;
-     
-    }
 
     
     }
 
+    function sonuc(){
+        console.log(toplam);
+        if(8<=toplam && toplam<=14){
+            
+        }else if(12<=toplam && toplam<=21){
+            console.log("jake");
+        }else if(16<=toplam && toplam<=28){
+            console.log("boyle");
+        }else if(20<=toplam && toplam<=35){
+            console.log("terry");
+        }
+    }
+ 
+
+
+function play() {
+    let audio1 = new Audio('https://seymakotan.github.io/JS_AdvProg/Term_project_99/theme.mp3');
+    audio1.play();
+}
+
 function gecisYap(){
     document.getElementById("ekran1").style.visibility = "hidden" ;
     document.getElementById("quizekran").style.visibility = "visible" ;
+    play();
     }
 
 degistir();
-
 
 
  
